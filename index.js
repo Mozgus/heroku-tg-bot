295c52c31e419624579e7e933f5e98cac85f5e09
Mozgus <retailnet@rambler.ru>
2018-08-04 15:04:00 +0300

    Show the OS type

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api')
+const os = require('os')
 
 const TOKEN = ''
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg=> {
-	bot.sendMessage(msg.chat.id, 'Hello from Heroku, bot says: "Hi, ${msg.from.first_name}"')
+	bot.sendMessage(msg.chat.id, `Hello from ${os.type}, bot says: Hi, "${msg.from.first_name}"`)
 })
